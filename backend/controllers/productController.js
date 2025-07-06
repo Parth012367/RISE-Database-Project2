5c9c9288d848d5cac3b6ef52135ea89004798135
Parth012367 <tiw.patty@gmail.com>
2025-07-06 12:10:17 +0530

    Add files via upload

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,1 +1,14 @@
+const Product = require('../models/productModel');
 
+// GET all products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+// POST a new product
+exports.createProduct = async (req, res) => {
+  const product = new Product(req.body);
+  const saved = await product.save();
+  res.status(201).json(saved);
+};
