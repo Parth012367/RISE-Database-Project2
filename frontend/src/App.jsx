a4be8e57d32b4866c0f9a5bf0a21b0e40a6c1dcc
Parth012367 <tiw.patty@gmail.com>
2025-07-06 12:18:43 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+import React from 'react';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div>
+      <h1>My E-Commerce App</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
