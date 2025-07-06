99c13d523d558124f0b1b09c17be6a3f49c7031d
Parth012367 <tiw.patty@gmail.com>
2025-07-06 12:13:26 +0530

    Add files via upload

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,1 +1,19 @@
+// backend/models/productModel.js
+const mongoose = require('mongoose');
 
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  category: String,
+  image: String,
+  reviews: [
+    {
+      username: String,
+      comment: String,
+      rating: Number
+    }
+  ]
+});
+
+module.exports = mongoose.model('Product', productSchema);
