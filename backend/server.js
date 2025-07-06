85873108b651b9702f73a662551420474d1fd2d4
Parth012367 <tiw.patty@gmail.com>
2025-07-06 12:09:07 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
